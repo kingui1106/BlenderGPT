dac5377ed188068583c55a2dd18443f2d23f5c10
wangshaobin <wangshaobin@southzn.com>
2023-03-29 11:54:14 +0800

    caddy 反向代理

--- lib/openai/__init__.py
+++ lib/openai/__init__.py
@@ -34,7 +34,7 @@ api_key = os.environ.get("OPENAI_API_KEY")
 api_key_path: Optional[str] = os.environ.get("OPENAI_API_KEY_PATH")
 
 organization = os.environ.get("OPENAI_ORGANIZATION")
-api_base = os.environ.get("OPENAI_API_BASE", "https://api.openai.com/v1")
+api_base = os.environ.get("OPENAI_API_BASE", "https://your/v1")
 api_type = os.environ.get("OPENAI_API_TYPE", "open_ai")
 api_version = (
     "2022-12-01" if api_type in ("azure", "azure_ad", "azuread") else None
